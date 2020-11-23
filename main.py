1fb988a04b081ff26f8ac29e407baf2aec77ff92
Tryfolos <70145934+Tryfolos@users.noreply.github.com>
2020-11-23 13:01:52 +0100

    change and difference

--- main.py
+++ main.py
@@ -1,2 +1,43 @@
 #################### Filip Hedman ####################
 
+#Importing shit.
+import os
+import time
+
+
+#Start of loop.
+while True:
+	print("[1] Räkna med addition")
+	print("[2] Räkna med subtraktion")
+	print("[3] Räkna med multiplikation")
+	print("[4] Avsluta")
+	print("[5] Avsluta Annorlunda")
+	choice = input("")
+	os.system("cls")
+
+#Code for adding numbers. Choice 1.
+	if choice == "1":
+		tal1 = input("Skriv in tal 1: ")
+		os.system("cls")
+		tal2 = input("Skriv in tal 2: ")
+		os.system("cls")
+		print(f"Summan för tal 1 och tal 2 är {int(tal1) + int(tal2)}")
+		time.sleep(2)
+
+
+
+#Code for exiting the program. This code is active when choice 4 or 5 is chosen.
+	if choice == "4":
+		print("Avslutar...")
+		time.sleep(2)
+		os.system("cls")
+		break
+
+	if choice == "5":
+		print("AVSLUTAR!!!")
+		time.sleep(2)
+		os.system("cls")
+		break
+
+
+
